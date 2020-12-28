2b7ef97fdb790b698c7205f95243d6769944881f
MARK ERICKSON <mark.atgithub@mre.org>
2020-12-27 23:16:36 -0800

    Included Cards from Material-UI

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -2,9 +2,40 @@ import { Link, routes } from '@redwoodjs/router'
 import { useState } from 'react'
 import { Form, TextField, Submit } from '@redwoodjs/forms'
 
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 
 const HomePage = () => {
-  const [lego, setLego] = useState()
+  const [lego, setLego] = useState();
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  
 
   const onSubmit = (data) => {
     console.info(data.setnumber)
@@ -71,8 +102,12 @@ const HomePage = () => {
 
       {lego && JSON.stringify(lego.results[0].part.part_img_url)}
       <div class="flex-container">
+      
         {lego &&
-          lego.results.map((lPart) => <LPart key={lPart.id} lpart={lPart} />)}
+          lego.results.map((lPart) => <Card className={classes.root}>
+          <CardContent><LPart key={lPart.id} lpart={lPart} /></CardContent>
+    </Card>)}
+            
       </div>
     </>
   )
